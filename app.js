0db47fe41104c457d7ad8ee90f06917bb2b5a149
Jack Kenna <jack.kenna@skybettingandgaming.com>
2016-09-20 09:21:53 +0100

    body parser added

--- app.js
+++ app.js
@@ -1,14 +1,65 @@
 
 var express = require('express');
 var app = express(); //constructor function 
+var body = require('bodyParser');
+
+app.listen(3000 , function(){
+  console.log('app running on port 3000');
+});
+
+app.use(function(req,res,next){
+
+
+	var users = 
+	[
+		{
+		"id": 0,
+		"name" : "Jack",
+		"age": 22
+
+		},
+		{
+		"id": 1,
+		"name" : "Laura",
+		"age": 21
+		},
+		{
+		"id": 2,
+		"name" : "Callum",
+		"age": 24
+		}
+	]
+
+	req.users = users;
+	next();
+});
 
 
 //index
-app.get("/user", function(req, res){
-	res.send("Ling");
+app.get("/users", function(req, res){
+	res.send(req.users);
 });
 
 //show
-app.get("/user/:id", function(req, res){
-	res.send("new");
+app.get("/users/:id", function(req, res){
+	var id = req.params.id;
+	res.send(req.users[id]);
+});
+3
+
+//show
+app.post("/users", function(req, res){
+
+	console.log(req.name);
+
+	var user = {
+		"id": req.id,
+		"name": req.name,
+		"age": req.age
+	};
+
+	console.log(user);
+	req.users.push(user);
+	console.log(req.users);
+
 });
